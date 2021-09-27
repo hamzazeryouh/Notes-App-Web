b1ad862f474243c30cf3ecfc00840987b28b67e4
hamza <zeryouhbusiness@gmail.com>
2021-09-27 19:34:56 +0200

    commite 2

--- Note-App-Web/src/app/app.module.ts
+++ Note-App-Web/src/app/app.module.ts
@@ -1,30 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestPipe } from './test.pipe';
 import { NotesAllComponent } from './notes-all/notes-all.component';
 import { NoteBookComponent } from './note-book/note-book.component';
-import { LikeComponent } from './like/like.component';
-import { RemoveNotesComponent } from './remove-notes/remove-notes.component';
-import { AddNoteComponent } from './add-note/add-note.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestPipe,
     NotesAllComponent,
-    NoteBookComponent,
-    LikeComponent,
-    RemoveNotesComponent,
-    AddNoteComponent
+    NoteBookComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
